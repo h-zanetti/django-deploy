0c0f1e66936d8c53fbe9b58a5fdc0125357b7be6
agah <henriaguiar99@gmail.com>
2021-04-28 19:52:04 -0300

    Changed INTERP in passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,7 @@
 from minimal_django_app.wsgi import application
+
+INTERP = "/home/wwagah/AgahSolutionsRepositories/minimal-django-app/venv/bin/python"
+
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+from django_project.wsgi import application
